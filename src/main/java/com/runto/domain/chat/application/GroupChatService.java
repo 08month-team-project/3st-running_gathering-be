317febff1277ef82305a885d77064d351b62ce2f
lsy28901 <tndus28901@gmail.com>
2024-10-25 13:11:27 +0900

    :sparkles: Feat(CHAT): 생성 메소드 삭제

--- src/main/java/com/runto/domain/chat/application/GroupChatService.java
+++ src/main/java/com/runto/domain/chat/application/GroupChatService.java
@@ -15,26 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class GroupChatService {
-    private final GroupChatRoomRepository groupChatRoomRepository;
     private final GroupChatRoomUserRepository groupChatRoomUserRepository;
-
-    // 그룹 채팅방 생성
-    @Transactional
-    public void createGroupChatRoom(Gathering gathering){
-        if (groupChatRoomRepository.existsByGathering(gathering)){
-            throw new ChatException(ErrorCode.CHATROOM_ALREADY_EXIST);
-        }
-        groupChatRoomRepository.save(GroupChatRoom.createRoom(gathering));
-    }
 
     // 그룹 채팅방 참여
     @Transactional
-    public void joinGroupChatRoom(User user, Long roomId){
-        //채팅방이 있는지 확인
-        GroupChatRoom groupChatRoom = groupChatRoomRepository.findById(roomId)
-                .orElseThrow(()->new ChatException(ErrorCode.CHATROOM_NOT_FOUND));
+    public void joinGroupChatRoom(User user, GroupChatRoom groupChatRoom){
 
-        //내가 그 채팅방에 참여중인지 확인 -> 스트림성능보다 쿼리가 더 좋을것 같다는 생각..?
+        //내가 그 채팅방에 참여중인지 확인
         if (groupChatRoomUserRepository.existsByGroupChatRoomAndUser(groupChatRoom,user)){
             throw new ChatException(ErrorCode.CHATROOM_ALREADY_JOINED);
         }
