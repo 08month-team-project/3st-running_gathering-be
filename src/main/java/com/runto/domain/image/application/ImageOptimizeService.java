a43960d587752a48ccf88b5950640853a6b81015
DokyungYou <willow74rr@gmail.com>
2024-10-25 19:57:52 +0900

    ♻️Refactor(IMAGE): ImageCompressService -> ImageOptimizeService 이름 변경, convertToWebpWithLossless 주석처리
    - convertToWebpWithLossless 의 경우 webp 로 바뀌는데 용량이 증가하였음
    - 썸네일, 본문이미지를 구분해서 작업을 진행했었는데, 하나로 작업하게끔 계획을 변경할 예정이어서
    일단은 convertToWebpWithLossless 를 사용하지 않게되어서 주석처리하였음
    - 무조건 압축되는 상황이 아니어서 압축이라는 축소적 의미보다는 좀 더 넓은 의미를 담게끔 바꾸었음
    
    Related to: #5

--- src/main/java/com/runto/domain/image/application/ImageOptimizeService.java
+++ src/main/java/com/runto/domain/image/application/ImageOptimizeService.java
@@ -10,10 +10,10 @@ import java.io.File;
 import static com.runto.global.exception.ErrorCode.IMAGE_CONVERSION_FAILED;
 
 @Service
-public class ImageCompressService {
+public class ImageOptimizeService {
 
     // 저장 경로는 수정될 여지가 많음
-    private final String filePath = "/images/webp/";
+    private final String filePath = "/images/";
 
     public File convertToWebp(String fileName, File originalFile) {
         try {
@@ -27,14 +27,14 @@ public class ImageCompressService {
     }
 
 
-    public File convertToWebpWithLossless(String fileName, File originalFile) {
-        try {
-            return ImmutableImage.loader()// 라이브러리 객체 생성
-                    .fromFile(originalFile) // 서버에 저장돼있는 이미지 파일 가져옴
-                    .output(WebpWriter.DEFAULT.withLossless(),  // 무손실 압축 설정, fileName.webp로 파일 생성
-                            new File(System.getProperty("user.dir") + filePath + fileName + ".webp"));
-        } catch (Exception e) {
-            throw new ImageException(e, IMAGE_CONVERSION_FAILED);
-        }
-    }
+//    public File convertToWebpWithLossless(String fileName, File originalFile) {
+//        try {
+//            return ImmutableImage.loader()// 라이브러리 객체 생성
+//                    .fromFile(originalFile) // 서버에 저장돼있는 이미지 파일 가져옴
+//                    .output(WebpWriter.DEFAULT.withLossless(),  // 무손실 압축 설정, fileName.webp로 파일 생성
+//                            new File(System.getProperty("user.dir") + filePath + fileName + ".webp"));
+//        } catch (Exception e) {
+//            throw new ImageException(e, IMAGE_CONVERSION_FAILED);
+//        }
+//    }
 }
