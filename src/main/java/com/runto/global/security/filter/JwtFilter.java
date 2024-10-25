cabf2f5a4257abab58568a4d3d07155768a88ab1
TestSeung <sjlim589@hanmail.net>
2024-10-25 15:28:28 +0900

    :sparkles: Feat(회원): JWTFilter 추가 Access 토큰관련 JWTFilter

--- src/main/java/com/runto/global/security/filter/JwtFilter.java
+++ src/main/java/com/runto/global/security/filter/JwtFilter.java
@@ -1,4 +1,70 @@
 package com.runto.global.security.filter;
 
-public class JwtFilter {
+import com.runto.global.security.detail.CustomUserDetails;
+import com.runto.global.security.dto.UserDetailsDTO;
+import com.runto.global.security.util.JWTUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = request.getHeader("access");
+
+        if(accessToken == null) {
+            System.out.println("Authorization header is missing");
+            filterChain.doFilter(request, response);
+            return;
+        }
+        //토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
+        try {
+            jwtUtil.isExpired(accessToken);
+        }catch (ExpiredJwtException e){
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.println("엑세스 토큰 만료");
+            //상태코드
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        //토큰이 access인지 확인(발급 시 페이로드에 명시)
+        String category = jwtUtil.getCategory(accessToken);
+
+        if(!category.equals("access")) {
+            PrintWriter writer = response.getWriter();
+            writer.print("엑세스토큰이 아닌데요?");
+
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        String username = jwtUtil.getUsername(accessToken);
+        String role = jwtUtil.getRole(accessToken);
+
+        UserDetailsDTO user = UserDetailsDTO.builder()
+                .nickname(username)
+                .role(role)
+                .build();
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
 }
