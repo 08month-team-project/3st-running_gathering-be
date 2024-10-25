c5d4603214c1e5a0854fea786837f31448b2695e
TestSeung <sjlim589@hanmail.net>
2024-10-25 15:27:55 +0900

    :sparkles: Feat(회원): 예외처리 추가

--- src/main/java/com/runto/global/exception/ErrorCode.java
+++ src/main/java/com/runto/global/exception/ErrorCode.java
@@ -12,8 +12,8 @@ public enum ErrorCode {
 
     IMAGE_SAVE_LIMIT_EXCEEDED(BAD_REQUEST, "이미지 저장 허용 개수를 넘었습니다."),
 
-    ALREADY_EXIST_USER(CONFLICT,"이미 존재하는 사용자입니다.");
-
+    ALREADY_EXIST_USER(CONFLICT,"이미 존재하는 사용자입니다."),
+    USER_NOT_FOUND(NOT_FOUND,"존재하지 않는 유저입니다." );
     private final HttpStatus httpStatus;
     private final String message;
 }
