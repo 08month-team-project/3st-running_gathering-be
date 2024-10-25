ab723990109f6b6f7d4563f53eb53575cbdffcb6
TestSeung <sjlim589@hanmail.net>
2024-10-25 15:29:52 +0900

    :sparkles: Feat(회원): Local계정 추가

--- src/main/java/com/runto/domain/user/dao/LocalAccountRepository.java
+++ src/main/java/com/runto/domain/user/dao/LocalAccountRepository.java
@@ -4,6 +4,9 @@ import com.runto.domain.user.domain.LocalAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LocalAccountRepository extends JpaRepository<LocalAccount, Long> {
+    Optional<LocalAccount> findByUserId(Long userId);;
 }
