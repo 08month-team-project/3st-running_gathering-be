a555fd8d4a42e24d1bb416c7dd6854743ba37675
DokyungYou <willow74rr@gmail.com>
2024-10-27 00:37:21 +0900

    ✨Feat(GATHERING): GatheringRepository - findGatheringDetailById 추가
    - Gathering 을 가지고 올 때, gatheringMembers - user 를 패치조인 하여 가져오는 메서드 구현
    
    Related to: #24

--- src/main/java/com/runto/domain/gathering/dao/GatheringRepository.java
+++ src/main/java/com/runto/domain/gathering/dao/GatheringRepository.java
@@ -3,8 +3,18 @@ package com.runto.domain.gathering.dao;
 
 import com.runto.domain.gathering.domain.Gathering;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface GatheringRepository extends JpaRepository<Gathering, Long> {
+
+    @Query("select g from Gathering g " +
+            " join fetch g.gatheringMembers gm " +
+            " join fetch gm.user " +
+            " where g.id= :gathering_id ")
+    Optional<Gathering> findGatheringDetailById(@Param("gathering_id") Long gatheringId);
 }
