4dc0d8fefdf040b112606f9d006768efd26fea90
DokyungYou <willow74rr@gmail.com>
2024-10-27 00:34:07 +0900

    ✨Feat(GATHERING): 모임 상세조회 기능 관련 dto 추가
    - GatheringResponse: 모임 자체에 대한 정보
    - GatheringMemberResponse: 모임에 속한 멤버정보
    - GatheringDetailResponse: 모임 상세조회 정보
    
    Related to: #24

---
+++ src/main/java/com/runto/domain/gathering/dto/GatheringMemberResponse.java
@@ -0,0 +1,39 @@
+package com.runto.domain.gathering.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Getter
+public class GatheringMemberResponse {
+
+    private Long gatheringMemberId;
+    private String nickname;
+    private String profileImageUrl;
+
+    public static List<GatheringMemberResponse> from(List<GatheringMember> gatheringMembers) {
+
+        return gatheringMembers.stream()
+                .map(GatheringMemberResponse::from)
+                .toList();
+    }
+
+    public static GatheringMemberResponse from(GatheringMember gatheringMember) {
+
+        return GatheringMemberResponse.builder()
+                .gatheringMemberId(gatheringMember.getId())
+                .nickname(gatheringMember.getUser().getNickname())
+                .profileImageUrl(gatheringMember.getUser().getProfileImageUrl())
+                .build();
+    }
+
+}
