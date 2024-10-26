cf5f8a67c5d8d795ba46e06add777d0928be7ac0
DokyungYou <willow74rr@gmail.com>
2024-10-27 00:43:43 +0900

    📝Comment(GATHERING): 모임글 등록 로직에 관한 TODO 코멘트 추가
    Related to: #4

--- src/main/java/com/runto/domain/gathering/application/GatheringService.java
+++ src/main/java/com/runto/domain/gathering/application/GatheringService.java
@@ -17,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.runto.domain.gathering.type.GatheringMemberRole.ORGANIZER;
@@ -36,11 +37,14 @@ public class GatheringService {
 
     // TODO: 회원관련 기능 dev에 머지되면 param 에 UserDetails 추가 & 교체 , user 관련 예외로 수정
     // TODO: 만약 신고기능 구현하는거면 나중에 관련 로직 추가 필요
+    // TODO: 날짜 설정 검증 로직 필요 (설정 날짜 관련 서비스 정책? 정하고 추후에 추가)
     @Transactional
     public void createGatheringGeneral(CreateGatheringRequest request) {
 
         User user = userRepository.findById(1L)
                 .orElseThrow(() -> new RuntimeException("없는 유저"));
+
+        validateDate(request.getDeadline(), request.getAppointedAt());
 
         Gathering gathering = request.toEntity(user);
         gathering.addMember(user, ORGANIZER);
@@ -52,6 +56,11 @@ public class GatheringService {
         // TODO moveImageProcess 에러 해결되면 주석 풀기
 //        imageService.moveImageFromTempToPermanent(request.getGatheringImageUrls()
 //                .getContentImageUrls());
+    }
+    
+    // TODO: 설정 날짜 관련 서비스 정책? 정하고 구현
+    private void validateDate(LocalDateTime deadLine, LocalDateTime appointedAt) {
+        return;
     }
 
     private void addContentImages(GatheringImageUrlsDto imageUrlDto, Gathering gathering) {
