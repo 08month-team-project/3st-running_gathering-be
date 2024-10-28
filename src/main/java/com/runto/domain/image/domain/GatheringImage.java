8552ea5a9657a638e2cb4b6fadbfc8f8394df614
DokyungYou <willow74rr@gmail.com>
2024-10-28 14:42:28 +0900

    ✨Feat(Image): GatheringImage, ImageUrlDto 에 메서드 추가
    - GatheringImage: getter 추가
    - ImageUrlDto: from 메서드 추가 [GatheringImage(엔티티) -> ImageUrlDto]
    
    Related to: #30

--- src/main/java/com/runto/domain/image/domain/GatheringImage.java
+++ src/main/java/com/runto/domain/image/domain/GatheringImage.java
@@ -5,12 +5,14 @@ import com.runto.domain.gathering.domain.Gathering;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
+@Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
