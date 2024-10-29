16f391555dce462e7185ba9b0ebfd8074e6901e8
유종화 <sola201@naver.com>
2024-10-29 22:00:54 +0900

    :sparkles(admin): 통계용 응답 컨틀로러 및 서비스 작성

--- src/main/java/com/runto/global/config/QuerydslConfig.java
+++ src/main/java/com/runto/global/config/QuerydslConfig.java
@@ -2,18 +2,18 @@ package com.runto.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@RequiredArgsConstructor
 @Configuration
-public class QuerydslConfig {
-    @PersistenceContext
-    private EntityManager entityManager;
+public class QueryDSLConfig {
+
+    private final EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);
     }
-
 }
