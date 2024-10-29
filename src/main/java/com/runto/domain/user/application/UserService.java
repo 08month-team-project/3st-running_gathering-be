dc10c72dc734f571c586cc74641e5d4de02ed86a
TestSeung <sjlim589@hanmail.net>
2024-10-29 23:04:12 +0900

    :sparkles: Feat(회원): email-check 로직작성

--- src/main/java/com/runto/domain/user/application/UserService.java
+++ src/main/java/com/runto/domain/user/application/UserService.java
@@ -2,14 +2,19 @@ package com.runto.domain.user.application;
 
 import com.runto.domain.user.dao.UserRepository;
 import com.runto.domain.user.domain.User;
+import com.runto.domain.user.dto.CheckEmailRequest;
 import com.runto.domain.user.dto.SignupRequest;
 import com.runto.domain.user.excepction.UserException;
+import com.runto.domain.user.type.UserStatus;
 import com.runto.global.exception.ErrorCode;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -33,5 +38,14 @@ public class UserService {
         User user = User.of(email,nickname,encodedPwd);
 
         userRepository.save(user);
+    }
+
+    @Transactional
+    public void checkEmailDuplicate(CheckEmailRequest checkEmailRequest){
+        Optional<User> findUser = userRepository.findByEmail(checkEmailRequest.getEmail());
+
+        if (findUser.isPresent() && findUser.get().getStatus().equals(UserStatus.ACTIVE)){
+            throw new UserException(ErrorCode.ALREADY_EXIST_USER);
+        }
     }
 }
