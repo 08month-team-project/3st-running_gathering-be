4ea97680562f4eeddcbadfa2013171455f212bd0
DokyungYou <willow74rr@gmail.com>
2024-10-29 17:56:49 +0900

    ✨Feat(GATHERING): 내 모임목록 조회 기능 구현
    - GatheringController
        - getMyGatherings
            - 유저정보, 페이지정보, 필터링, 정렬을 위한 파람값들을 받아옴
            - TODO userId -> userDetails 로 바꿔야함
    - GatheringService
        - getUserGatherings
            - 내 모임목록에서만 쓰이지 않고 특정유저의 모임목록을 조회할 수도 있을 상황을 고려하여 getMy 가 아닌 getUser 의 접두사로 네이밍하였음
    
    Related to: #34

--- src/main/java/com/runto/domain/gathering/api/GatheringController.java
+++ src/main/java/com/runto/domain/gathering/api/GatheringController.java
@@ -3,11 +3,17 @@ package com.runto.domain.gathering.api;
 import com.runto.domain.gathering.application.GatheringService;
 import com.runto.domain.gathering.dto.CreateGatheringRequest;
 import com.runto.domain.gathering.dto.GatheringDetailResponse;
+import com.runto.domain.gathering.dto.UserGatheringsRequestParams;
+import com.runto.domain.gathering.dto.UserGatheringsResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/gatherings")
@@ -27,6 +33,19 @@ public class GatheringController {
     public ResponseEntity<GatheringDetailResponse> getGatheringDetail(
             @PathVariable("gathering_id") Long gatheringId) {
         return ResponseEntity.ok(gatheringService.getGatheringDetail(gatheringId));
+    }
+
+
+    // TODO userId -> userDetails 로 바꿔야함
+    @GetMapping
+    public ResponseEntity<UserGatheringsResponse> getMyGatherings(
+            @RequestParam(name = "user_id") Long userId, // before 유저인증 적용
+            @PageableDefault(size = 8) Pageable pageable,
+            @Valid @ModelAttribute UserGatheringsRequestParams requestParams
+    ) {
+
+        return ResponseEntity.ok(gatheringService
+                .getUserGatherings(userId, pageable, requestParams));
     }
 
 }
