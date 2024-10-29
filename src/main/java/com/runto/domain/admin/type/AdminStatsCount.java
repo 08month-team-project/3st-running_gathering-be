3f7e5288fbf07bb1019c628c591203ac4dca92b6
유종화 <sola201@naver.com>
2024-10-29 21:52:19 +0900

    :sparkles(admin): 통계들을 위해 생성한 이넘 클래스

---
+++ src/main/java/com/runto/domain/admin/type/AdminStatsCount.java
@@ -0,0 +1,14 @@
+package com.runto.domain.admin.type;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.STRING)
+public enum AdminStatsCount {
+
+    TOTAL,
+    REPORTED,
+    BLACKLIST;
+
+}
