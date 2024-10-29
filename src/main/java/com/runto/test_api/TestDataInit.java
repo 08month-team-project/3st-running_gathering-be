2b00337c8b1d5f494753bce15a7279a57eb2b7e7
DokyungYou <willow74rr@gmail.com>
2024-10-29 17:57:25 +0900

    ✨Feat(GATHERING): TestDataInit 수정(데이터 추가)
    Related to: #34

--- src/main/java/com/runto/test_api/TestDataInit.java
+++ src/main/java/com/runto/test_api/TestDataInit.java
@@ -1,5 +1,10 @@
 package com.runto.test_api;
 
+import com.runto.domain.gathering.dao.GatheringRepository;
+import com.runto.domain.gathering.domain.Gathering;
+import com.runto.domain.gathering.domain.Location;
+import com.runto.domain.gathering.type.GoalDistance;
+import com.runto.domain.gathering.type.RunningConcept;
 import com.runto.domain.user.dao.LocalAccountRepository;
 import com.runto.domain.user.dao.UserRepository;
 import com.runto.domain.user.domain.LocalAccount;
@@ -11,6 +16,12 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.runto.domain.gathering.type.GatheringMemberRole.ORGANIZER;
+import static com.runto.domain.gathering.type.GatheringMemberRole.PARTICIPANT;
 import static com.runto.domain.user.type.Gender.WOMAN;
 
 @RequiredArgsConstructor
@@ -20,26 +31,99 @@ public class TestDataInit {
     private final UserRepository userRepository;
     private final LocalAccountRepository localAccountRepository;
 
+    private final GatheringRepository gatheringRepository;
+
     @EventListener(ApplicationReadyEvent.class)
     @Transactional
     public void init() {
 
+        List<User> users = new ArrayList<>();
 
-        LocalAccount localAccount = LocalAccount.builder()
-                .password("123456")
-                .build();
-        User user = User.builder()
-                .email("runto@gmail.com")
-                .name("임시유저")
-                .nickname("임시유저")
-                .gender(WOMAN)
-                //.status()
-                .localAccount(localAccount)
-                .role(UserRole.USER)
-                //.profileImageUrl()
-                .build();
+        for (int i = 0; i < 10; i++) {
+            LocalAccount localAccount = LocalAccount.builder()
+                    .password("123456")
+                    .build();
 
-        userRepository.save(user);
-        localAccountRepository.save(localAccount);
+            users.add(User.builder()
+                    .email("runto@gmail.com")
+                    .name("테스트유저이름" + i + 1)
+                    .nickname("테스트유저닉네임" + i + 1)
+                    .gender(WOMAN)
+                    //.status()
+                    .localAccount(localAccount)
+                    .role(UserRole.USER)
+                    .profileImageUrl(i + 1 + "번유저 썸네일 url")
+                    .build());
+        }
+        users = userRepository.saveAll(users);
+
+
+        List<Gathering> gatherings = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Gathering gathering = Gathering.builder()
+                    .organizerId(users.get(i).getId())
+                    .title("(기한 남음) 우리 모두 달립시다 " + i)
+                    .description("재밌게 달려봅시다." + i)
+                    .appointedAt(LocalDateTime.now().plusDays(i + 2))
+                    .deadline(LocalDateTime.now().plusDays(i + 1))
+                    .concept(RunningConcept.HEALTH)
+                    .goalDistance(GoalDistance.FREE)
+                    .thumbnailUrl(i + "번 모임 썸네일")
+                    //.hits(0)
+                    .location(Location.of("우리집 앞", 0.0, 0.0))
+                    //.status(NORMAL)
+                    .maxNumber(10)
+                    .currentNumber(1)
+                    .build();
+
+            gathering.addMember(users.get(i), ORGANIZER);
+
+            for (int j = 0; j < 10; j++) {
+                if (j == i) continue;
+
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
+                    gathering.addMember(users.get(j), PARTICIPANT);
+                }
+            }
+
+            gatherings.add(gathering);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Gathering gathering = Gathering.builder()
+                    .organizerId(users.get(i).getId())
+                    .title("(기한 만료) 우리 모두 달립시다 " + i)
+                    .description("재밌게 달려봅시다." + i)
+                    .appointedAt(LocalDateTime.now().minusDays(i + 2))
+                    .deadline(LocalDateTime.now().minusDays(i + 1))
+                    .concept(RunningConcept.HEALTH)
+                    .goalDistance(GoalDistance.FREE)
+                    .thumbnailUrl(i + "번 모임 썸네일")
+                    //.hits(0)
+                    .location(Location.of("우리집 앞", 0.0, 0.0))
+                    //.status(NORMAL)
+                    .maxNumber(10)
+                    .currentNumber(1)
+                    .build();
+
+            gathering.addMember(users.get(i), ORGANIZER);
+
+            for (int j = 0; j < 10; j++) {
+                if (j == i) continue;
+
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
+                    gathering.addMember(users.get(j), PARTICIPANT);
+                }
+            }
+
+            gatherings.add(gathering);
+        }
+
+
+
+        gatherings = gatheringRepository.saveAll(gatherings);
+
+
     }
 }
