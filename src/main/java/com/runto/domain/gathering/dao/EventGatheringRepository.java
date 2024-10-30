39693c576e10a9b16ad4d97ec0d9b39d5cd97388
DokyungYou <willow74rr@gmail.com>
2024-10-30 19:38:16 +0900

    ✨Feat(GATHERING): EventGatheringRepository - findEventGatheringsByUserId 추가
    - EventGathering 를 Gathering과 패치조인하여 Slice로 반환
    
    Related to: #57

--- src/main/java/com/runto/domain/gathering/dao/EventGatheringRepository.java
+++ src/main/java/com/runto/domain/gathering/dao/EventGatheringRepository.java
@@ -1,9 +1,19 @@
 package com.runto.domain.gathering.dao;
 
 import com.runto.domain.gathering.domain.EventGathering;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventGatheringRepository extends JpaRepository<EventGathering, Long> {
+
+    @Query("select e from EventGathering e " +
+            " join fetch e.gathering g" +
+            " where g.organizerId = :user_id")
+    Slice<EventGathering> findEventGatheringsByUserId(
+            @Param("user_id") Long userId, Pageable pageable);
 }
