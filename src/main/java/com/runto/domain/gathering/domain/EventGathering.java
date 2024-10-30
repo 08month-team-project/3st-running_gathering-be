9bd3c18b712cc6de58ff4d271b868c23fa1dbdaf
DokyungYou <willow74rr@gmail.com>
2024-10-31 03:18:35 +0900

    ♻️Refactor(GATHERING): EventGathering - @ Table, @ Column 적용
    Related to: #62

--- src/main/java/com/runto/domain/gathering/domain/EventGathering.java
+++ src/main/java/com/runto/domain/gathering/domain/EventGathering.java
@@ -12,11 +12,13 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Getter
 @NoArgsConstructor
+@Table(name = "event_gathering")
 @Entity
 public class EventGathering extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "event_gathering_id")
     private Long id;
 
     // oneToOne 양방향일 경우 주인이 아닌 쪽을 가져올때, LAZY 가 작동안하지만 EventGathering 를 가져올땐,
