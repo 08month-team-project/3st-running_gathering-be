48ae5cea0fbe1d663d369f16442c6bfe908ae458
lsy28901 <tndus28901@gmail.com>
2024-10-30 15:03:10 +0900

    :sparkles: Feat(CHAT): 1:1채팅하기 로직을 기존에는 생성, 조회를 나눠서 처리했으나 한번의 요청에 같이 진행되도록 수정함, 1:1 채팅 목록보기 로직 구현

--- src/main/java/com/runto/domain/chat/application/DirectChatService.java
+++ src/main/java/com/runto/domain/chat/application/DirectChatService.java
@@ -2,6 +2,8 @@ package com.runto.domain.chat.application;
 
 import com.runto.domain.chat.dao.DirectChatRoomRepository;
 import com.runto.domain.chat.domain.DirectChatRoom;
+import com.runto.domain.chat.dto.DirectChatInfoDTO;
+import com.runto.domain.chat.dto.DirectChatRoomResponse;
 import com.runto.domain.chat.exception.ChatException;
 import com.runto.domain.user.dao.UserRepository;
 import com.runto.domain.user.domain.User;
@@ -9,15 +11,26 @@ import com.runto.domain.user.excepction.UserException;
 import com.runto.global.exception.ErrorCode;
 import jakarta.persistence.OptimisticLockException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DirectChatService {
     private final UserRepository userRepository;
     private final DirectChatRoomRepository directChatRoomRepository;
+
+    @Transactional
+    public DirectChatInfoDTO createAndGetDirectChat(String email,Long otherId){
+        createDirectChat(email, otherId);
+        return getDirectChatInfo(email, otherId);
+    }
 
     @Transactional
     public void createDirectChat(String email, Long otherId){
@@ -26,21 +39,44 @@ public class DirectChatService {
         User otherUser = userRepository.findById(otherId)
                 .orElseThrow(()->new UserException(ErrorCode.USER_NOT_FOUND));
 
+        //A와 B의 채팅방이 이미 존재하는지 확인
+        if (directChatRoomRepository.existsByUserPair(me,otherUser)){
+            return;
+        }
+
         //동일한 아이디의 채팅방 생성 막기
         if (me.getId().equals(otherUser.getId())){
             throw new ChatException(ErrorCode.CHATROOM_CREATE_FAILED_OWN);
-        }
-
-        //A와 B의 채팅방이 이미 존재하는지 확인
-        if (directChatRoomRepository.existsByUserPair(me,otherUser)){
-            throw new ChatException(ErrorCode.CHATROOM_ALREADY_EXIST);
         }
 
         DirectChatRoom directChatRoom = DirectChatRoom.createRoom(me,otherUser);
         try {
             directChatRoomRepository.save(directChatRoom);
         }catch (OptimisticLockException | DataIntegrityViolationException e){
-            throw new ChatException(ErrorCode.CHATROOM_ALREADY_EXIST);
+            return;
         }
     }
+
+    @Transactional
+    public DirectChatInfoDTO getDirectChatInfo(String email, Long otherId){
+        User me = userRepository.findByEmail(email)
+                .orElseThrow(()->new UserException(ErrorCode.USER_NOT_FOUND));
+        User otherUser = userRepository.findById(otherId)
+                .orElseThrow(()->new UserException(ErrorCode.USER_NOT_FOUND));
+
+        DirectChatRoom directChatRoom = directChatRoomRepository.findByUserPair(me,otherUser)
+                .orElseThrow(()->new ChatException(ErrorCode.CHATROOM_NOT_FOUND));
+
+        return DirectChatInfoDTO.of(directChatRoom.getId(),me.getId(),otherUser.getId());
+    }
+
+    public Slice<DirectChatRoomResponse> getDirectChatRoomList(String email,int pageNum,int size){
+        User me = userRepository.findByEmail(email)
+                .orElseThrow(()->new UserException(ErrorCode.USER_NOT_FOUND));
+
+        Pageable pageable = PageRequest.of(pageNum,size);
+
+        return directChatRoomRepository.getChatRooms(me.getId(), pageable);
+    }
+
 }
