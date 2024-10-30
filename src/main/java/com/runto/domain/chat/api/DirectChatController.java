6ff210a9922d46689cb41035bd342462474ca823
lsy28901 <tndus28901@gmail.com>
2024-10-30 14:59:23 +0900

    :sparkles: Feat(CHAT): 1:1 채팅하기 api 컨트롤러 매핑 및 수정, 1:1 채팅방 목록조회 매핑

--- src/main/java/com/runto/domain/chat/api/DirectChatController.java
+++ src/main/java/com/runto/domain/chat/api/DirectChatController.java
@@ -1,15 +1,16 @@
 package com.runto.domain.chat.api;
 
 import com.runto.domain.chat.application.DirectChatService;
+import com.runto.domain.chat.dto.DirectChatInfoDTO;
+import com.runto.domain.chat.dto.DirectChatRoomResponse;
 import com.runto.global.security.detail.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -18,11 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class DirectChatController {
     private final DirectChatService directChatService;
 
-    //1:1 채팅방 생성 api
-    @PostMapping
-    public ResponseEntity<Void> createDirectChatRoom(@RequestParam(name = "other_id") Long otherId,
-                                               @AuthenticationPrincipal CustomUserDetails userDetails){
-        directChatService.createDirectChat(userDetails.getUsername(), otherId);
+    //1:1 채팅방 1:1채팅하기로 조회(생성과 같이 있음)
+    @GetMapping
+    public ResponseEntity<DirectChatInfoDTO> getDirectChatRoom(@RequestParam(name = "other_id") Long otherId,
+                                                  @AuthenticationPrincipal CustomUserDetails userDetails){
+        DirectChatInfoDTO directChatInfoDTO = directChatService.createAndGetDirectChat(userDetails.getUsername(),otherId);
+        //TODO 채팅방 정보를 통한 이전 채팅 메시지 보여주기
+        return ResponseEntity.ok(directChatInfoDTO);
+    }
+
+    //1:1 채팅방 목록 조회
+    @GetMapping("/list")
+    public ResponseEntity<Slice<DirectChatRoomResponse>> getDirectChatRoomList(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                                               @RequestParam(name = "page_num") int pageNum,
+                                                                               @RequestParam(defaultValue = "7") int size){
+        return ResponseEntity.ok(directChatService.getDirectChatRoomList(userDetails.getUsername(), pageNum,size));
+    }
+
+    //TODO 1:1 채팅방 목록에서 눌러서 채팅방 조회
+    @GetMapping("/{room_id}")
+    public ResponseEntity<Void> getDirectChatRoomFromList(@PathVariable(name = "room_id") Long roomId){
+        //TODO 채팅방 정보를 통한 이전 채팅 메시지 보여주기
         return ResponseEntity.ok().build();
     }
 
