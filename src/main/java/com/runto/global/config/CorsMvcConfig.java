d7a00eb4e8e0a10500ab8ed2710d9994905f60a9
TestSeung <sjlim589@hanmail.net>
2024-10-31 23:54:19 +0900

    :bug:  Feat(문서): swagger Cors 문제

--- src/main/java/com/runto/global/config/CorsMvcConfig.java
+++ src/main/java/com/runto/global/config/CorsMvcConfig.java
@@ -1,17 +1,22 @@
 package com.runto.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
+    @Value("${servername}")
+    private String backendServerName;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .exposedHeaders("Set-Cookie")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(backendServerName)
 //                .allowedOrigins("https://runto.vercel.app/")
                 .allowCredentials(true);
 
