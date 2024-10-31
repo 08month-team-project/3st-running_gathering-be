34dd7528cb03485dccc26723d7a119a51941cfc1
DokyungYou <willow74rr@gmail.com>
2024-11-01 01:42:55 +0900

    ♻️Refactor(GATHERING): UserGatheringsResponse- e9ec55ee 에서 삭제한 클래스 import문 삭제
    Related to: #34

--- src/main/java/com/runto/domain/gathering/dto/UserGatheringsResponse.java
+++ src/main/java/com/runto/domain/gathering/dto/UserGatheringsResponse.java
@@ -3,7 +3,6 @@ package com.runto.domain.gathering.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.runto.domain.gathering.domain.Gathering;
-import com.runto.domain.gathering.type.GatheringResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
