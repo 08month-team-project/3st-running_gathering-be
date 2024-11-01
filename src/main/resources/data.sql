4472c7b9f63fd7361e5252d6bf0e9dc47848c54e
유종화 <sola201@naver.com>
2024-11-02 00:26:26 +0900

    :sparkles: 모임 더미 테이터 추가및 신고 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,7 +16,7 @@ VALUES
 ('user08@example.com', NULL, 'User08', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-02-05 00:00:00', '2024-02-05 00:00:00'),
 ('user09@example.com', NULL, 'User09', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-02-10 00:00:00', '2024-02-10 00:00:00'),
 ('user10@example.com', NULL, 'User10', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-02-15 00:00:00', '2024-02-15 00:00:00'),
-('user11@example.com', NULL, 'User11', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-02-20 00:00:00', '2024-02-20 00:00:00');
+('user11@example.com', NULL, 'User11', 'NONE', 'REPORTED', 'USER', NULL, NULL, '2024-02-20 00:00:00', '2024-02-20 00:00:00');
 
 INSERT INTO users (email, name, nickname, gender, user_status, user_role, profile_image_url, local_id, created_at, modified_at)
 VALUES
@@ -27,12 +27,12 @@ VALUES
 ('user15@example.com', NULL, 'User15', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-03-15 00:00:00', '2024-03-15 00:00:00'),
 ('user16@example.com', NULL, 'User16', 'NONE', 'DISABLED', 'USER', NULL, NULL, '2024-03-20 00:00:00', '2024-03-20 00:00:00'),
 ('user17@example.com', NULL, 'User17', 'NONE', 'DISABLED', 'USER', NULL, NULL, '2024-03-20 00:00:00', '2024-03-20 00:00:00'),
-('user18@example.com', NULL, 'User18', 'NONE', 'DISABLED', 'USER', NULL, NULL, '2024-03-20 00:00:00', '2024-03-20 00:00:00');
+('user18@example.com', NULL, 'User18', 'NONE', 'REPORTED', 'USER', NULL, NULL, '2024-03-20 00:00:00', '2024-03-20 00:00:00');
 
 INSERT INTO users (email, name, nickname, gender, user_status, user_role, profile_image_url, local_id, created_at, modified_at)
 VALUES
 -- 4월 사용자
-('user19@example.com', NULL, 'User19', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-04-01 00:00:00', '2024-04-01 00:00:00'),
+('user19@example.com', NULL, 'User19', 'NONE', 'REPORTED', 'USER', NULL, NULL, '2024-04-01 00:00:00', '2024-04-01 00:00:00'),
 ('user20@example.com', NULL, 'User20', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-04-05 00:00:00', '2024-04-05 00:00:00'),
 ('user21@example.com', NULL, 'User21', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-04-10 00:00:00', '2024-04-10 00:00:00'),
 ('user22@example.com', NULL, 'User22', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-04-15 00:00:00', '2024-04-15 00:00:00'),
@@ -46,13 +46,13 @@ VALUES
 INSERT INTO users (email, name, nickname, gender, user_status, user_role, profile_image_url, local_id, created_at, modified_at)
 VALUES
 -- 5월 사용자
-('user29@example.com', NULL, 'User29', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-05-01 00:00:00', '2024-05-01 00:00:00'),
+('user29@example.com', NULL, 'User29', 'NONE', 'REPORTED', 'USER', NULL, NULL, '2024-05-01 00:00:00', '2024-05-01 00:00:00'),
 ('user30@example.com', NULL, 'User30', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-05-05 00:00:00', '2024-05-05 00:00:00'),
 ('user31@example.com', NULL, 'User31', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-05-10 00:00:00', '2024-05-10 00:00:00'),
 ('user32@example.com', NULL, 'User32', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-05-15 00:00:00', '2024-05-15 00:00:00'),
 ('user33@example.com', NULL, 'User33', 'NONE', 'DISABLED', 'USER', NULL, NULL, '2024-05-20 00:00:00', '2024-05-20 00:00:00'),
 ('user34@example.com', NULL, 'User34', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-05-01 00:00:00', '2024-05-01 00:00:00'),
-('user35@example.com', NULL, 'User35', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-05-05 00:00:00', '2024-05-05 00:00:00'),
+('user35@example.com', NULL, 'User35', 'NONE', 'REPORTED', 'USER', NULL, NULL, '2024-05-05 00:00:00', '2024-05-05 00:00:00'),
 ('user36@example.com', NULL, 'User36', 'NONE', 'BANNED', 'USER', NULL, NULL, '2024-05-10 00:00:00', '2024-05-10 00:00:00'),
 ('user37@example.com', NULL, 'User37', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-05-15 00:00:00', '2024-05-15 00:00:00'),
 ('user38@example.com', NULL, 'User38', 'NONE', 'DISABLED', 'USER', NULL, NULL, '2024-05-20 00:00:00', '2024-05-20 00:00:00');
@@ -61,7 +61,7 @@ INSERT INTO users (email, name, nickname, gender, user_status, user_role, profil
 VALUES
 -- 6월 사용자
 ('user39@example.com', NULL, 'User39', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-06-01 00:00:00', '2024-06-01 00:00:00'),
-('user40@example.com', NULL, 'User40', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-06-05 00:00:00', '2024-06-05 00:00:00'),
+('user40@example.com', NULL, 'User40', 'NONE', 'REPORTED', 'USER', NULL, NULL, '2024-06-05 00:00:00', '2024-06-05 00:00:00'),
 ('user41@example.com', NULL, 'User41', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-06-10 00:00:00', '2024-06-10 00:00:00'),
 ('user42@example.com', NULL, 'User42', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-06-15 00:00:00', '2024-06-15 00:00:00'),
 ('user43@example.com', NULL, 'User43', 'NONE', 'DISABLED', 'USER', NULL, NULL, '2024-06-20 00:00:00', '2024-06-20 00:00:00'),
@@ -77,14 +77,14 @@ VALUES
 ('user49@example.com', NULL, 'User49', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-07-01 00:00:00', '2024-07-01 00:00:00'),
 ('user50@example.com', NULL, 'User50', 'NONE', 'BANNED', 'USER', NULL, NULL, '2024-07-05 00:00:00', '2024-07-05 00:00:00'),
 ('user51@example.com', NULL, 'User51', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-07-10 00:00:00', '2024-07-10 00:00:00'),
-('user52@example.com', NULL, 'User52', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-07-15 00:00:00', '2024-07-15 00:00:00'),
+('user52@example.com', NULL, 'User52', 'NONE', 'REPORTED', 'USER', NULL, NULL, '2024-07-15 00:00:00', '2024-07-15 00:00:00'),
 ('user53@example.com', NULL, 'User53', 'NONE', 'DISABLED', 'USER', NULL, NULL, '2024-07-20 00:00:00', '2024-07-20 00:00:00');
 
 INSERT INTO users (email, name, nickname, gender, user_status, user_role, profile_image_url, local_id, created_at, modified_at)
 VALUES
 -- 8월 사용자
 ('user54@example.com', NULL, 'User54', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-08-01 00:00:00', '2024-08-01 00:00:00'),
-('user55@example.com', NULL, 'User55', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-08-05 00:00:00', '2024-08-05 00:00:00'),
+('user55@example.com', NULL, 'User55', 'NONE', 'REPORTED', 'USER', NULL, NULL, '2024-08-05 00:00:00', '2024-08-05 00:00:00'),
 ('user56@example.com', NULL, 'User56', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-08-10 00:00:00', '2024-08-10 00:00:00'),
 ('user57@example.com', NULL, 'User57', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-08-15 00:00:00', '2024-08-15 00:00:00'),
 ('user58@example.com', NULL, 'User58', 'NONE', 'DISABLED', 'USER', NULL, NULL, '2024-08-20 00:00:00', '2024-08-20 00:00:00');
@@ -94,9 +94,9 @@ VALUES
 -- 9월 사용자
 ('user59@example.com', NULL, 'User59', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-09-01 00:00:00', '2024-09-01 00:00:00'),
 ('user60@example.com', NULL, 'User60', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-09-05 00:00:00', '2024-09-05 00:00:00'),
-('user61@example.com', NULL, 'User61', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-09-10 00:00:00', '2024-09-10 00:00:00'),
+('user61@example.com', NULL, 'User61', 'NONE', 'REPORTED', 'USER', NULL, NULL, '2024-09-10 00:00:00', '2024-09-10 00:00:00'),
 ('user62@example.com', NULL, 'User62', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-09-15 00:00:00', '2024-09-15 00:00:00'),
-('user63@example.com', NULL, 'User63', 'NONE', 'ACTIVE', 'USER', NULL, NULL, '2024-09-20 00:00:00', '2024-09-20 00:00:00');
+('user63@example.com', NULL, 'User63', 'NONE', 'REPORTED', 'USER', NULL, NULL, '2024-09-20 00:00:00', '2024-09-20 00:00:00');
 
 INSERT INTO black_list (user_id, cumulative_number ,expires_at)
 VALUES
@@ -107,6 +107,19 @@ VALUES
 (36,36,'2024-09-04 00:00:00'),
 (44,5,'2024-09-04 00:00:00'),
 (50,5,'2024-09-04 00:00:00');
+
+INSERT into report (user_id, report_reason, description, created_at, modified_at)
+values
+(11, 'ILLEGAL_CONTENT', '이사람 자꾸 이상한 정보로 사람 불편하게 해요!!!', '2024-01-01 18:00:00','2024-01-01 18:00:00'),
+(18, 'INSULT_OR_ABUSE', '욕설을 자꾸 하면 사람 기만합니다.', '2024-02-01 18:00:00','2024-01-01 18:00:00'),
+(19, 'ILLEGAL_CONTENT', '이사람 자꾸 이상한 정보로 사람 불편하게 해요!!!', '2024-03-23 18:00:00','2024-01-01 18:00:00'),
+(29, 'COMMERCIAL_PROMOTION', '홍보..좀.. 그만 ', '2024-04-21 18:00:00','2024-01-01 18:00:00'),
+(35, 'ILLEGAL_CONTENT', '불법 도박 사이트 자꾸 올립니다.', '2024-05-22 18:00:00','2024-01-01 18:00:00'),
+(40, 'OBSCENE_CONTENT', '이상한 주소로 자꾸 안내해요', '2024-06-01 18:00:00','2024-01-01 18:00:00'),
+(52, 'DUPLICATE_CONTENT', '같은 글을 계속 작성해서 불편해요', '2024-07-01 18:00:00','2024-01-01 18:00:00'),
+(55, 'ILLEGAL_CONTENT', '이사람 자꾸 이상한 정보로 사람 불편하게 해요!!!', '2024-08-01 18:00:00','2024-01-01 18:00:00'),
+(61, 'PERSONAL_INFO_EXPOSURE', '개인정보 자꾸 노출해요 ㅠㅠ', '2024-09-01 18:00:00','2024-01-01 18:00:00'),
+(63, 'FREQUENT_NO_SHOW', '상습적인 프로 불참러입니다.', '2024-10-01 18:00:00','2024-01-01 18:00:00');
 
 INSERT INTO gatherings (b_code, current_number, h_code, max_number, x, y, appointed_at, created_at, deadline, event_gathering_id, hits, modified_at, organizer_id, title, description, address_name, region_1depth_name, region_2depth_name, region_3depth_name, thumbnail_url, concept, gathering_type, goal_distance, status)
 VALUES
