e119b9c04367cb386b7a9ace029f97ebee6a0f2c
DokyungYou <willow74rr@gmail.com>
2024-11-02 19:19:06 +0900

    ✨Feat(GATHERING): 회원 프로필 조회 반환 dto
    Related to: #97

---
+++ src/main/java/com/runto/domain/user/dto/UserProfileResponse.java
@@ -0,0 +1,39 @@
+package com.runto.domain.user.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.runto.domain.user.domain.User;
+import com.runto.domain.user.type.Gender;
+import com.runto.domain.user.type.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UserProfileResponse {
+
+    private String email;
+    private String name;
+    private Gender gender;
+    private UserStatus status;
+    private LocalDateTime singupAt;
+    private String profileUrl;
+
+
+    public static UserProfileResponse from(User user) {
+        return UserProfileResponse
+                .builder()
+                .email(user.getEmail())
+                .name(user.getName())
+                .gender(user.getGender())
+                .status(user.getStatus())
+                .singupAt(user.getCreatedAt())
+                .profileUrl(user.getProfileImageUrl())
+                .build();
+    }
+}
