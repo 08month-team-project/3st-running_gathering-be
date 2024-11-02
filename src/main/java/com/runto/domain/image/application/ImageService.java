b499c89e8babc0c310d84cadb6fc09eb3a4d5a8b
DokyungYou <willow74rr@gmail.com>
2024-11-03 02:49:22 +0900

    ✨Feat(GATHERING): ImageService - updateUserProfile 추가
    - S3에 있는 기존의 프로필 이미지를 삭제
    - 새로운 이미지를 업로드 후 dto 반환
    - 요청하는 이미지가 null 값이라면 null을 넣어서 반환
    
    related: #96

--- src/main/java/com/runto/domain/image/application/ImageService.java
+++ src/main/java/com/runto/domain/image/application/ImageService.java
@@ -1,12 +1,11 @@
 package com.runto.domain.image.application;
 
 import com.runto.domain.image.dao.GatheringImageRepository;
-import com.runto.domain.image.dto.ImageRegisterResponse;
-import com.runto.domain.image.dto.ImageUploadRequest;
-import com.runto.domain.image.dto.ImageUrlDto;
+import com.runto.domain.image.dto.*;
 import com.runto.domain.image.type.ImageUrlsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -14,15 +13,15 @@ import java.util.List;
 @Service
 public class ImageService {
 
-    private final S3GatheringImageService s3GatheringImageService;
+    private final S3ImageService s3ImageService;
     private final GatheringImageRepository gatheringImageRepository;
 
     public ImageRegisterResponse registerGatheringImages(ImageUploadRequest uploadRequest) {
 
         if (uploadRequest == null) return null;
 
-        List<ImageUrlDto> contentImageUrls = s3GatheringImageService
-                .uploadContentImages(uploadRequest.getImages());
+        List<ImageUrlDto> contentImageUrls = s3ImageService
+                .uploadContentImages(uploadRequest.getImages(), "gathering");
 
         return ImageRegisterResponse.of(
                 uploadRequest.getRepresentativeImageIndex(), contentImageUrls);
@@ -33,7 +32,7 @@ public class ImageService {
         if (contentImageUrls == null || contentImageUrls.isEmpty()) return;
 
         contentImageUrls.forEach(imageUrlDto ->
-                s3GatheringImageService.moveImageProcess(imageUrlDto.getImageUrl()));
+                s3ImageService.moveImageProcess(imageUrlDto.getImageUrl()));
     }
 
     public ImageUrlsResponse getGatheringImages(Long gatheringId) {
@@ -44,4 +43,17 @@ public class ImageService {
                         .toList());
     }
 
+
+    public ImageUrlDto updateUserProfile(Long userId, MultipartFile newImageFile, String oldImageUrl) {
+
+        s3ImageService.deleteS3Object(oldImageUrl, "temp/"); // TODO 나중에 이미지 버그 수정 및 리팩토링 때 수정될 수도 있음
+
+        if (newImageFile == null || newImageFile.isEmpty()) {
+            return new ImageUrlDto(null, 0);
+        }
+
+        return s3ImageService.uploadContentImages(
+                List.of(new ImageDto(newImageFile, 0)),
+                        String.format("profile[%s]", userId)).get(0);
+    }
 }
