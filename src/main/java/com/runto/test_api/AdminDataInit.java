c9d1bd35e74b8972f8cf8e61d41ad7dc3d2fc233
유종화 <sola201@naver.com>
2024-11-05 17:53:42 +0900

    :sparkles: 관리자용 아이디 추가

---
+++ src/main/java/com/runto/test_api/AdminDataInit.java
@@ -0,0 +1,45 @@
+package com.runto.test_api;
+
+import com.runto.domain.user.dao.UserRepository;
+import com.runto.domain.user.domain.LocalAccount;
+import com.runto.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.runto.domain.user.type.Gender.NONE;
+import static com.runto.domain.user.type.UserRole.ADMIN;
+
+@RequiredArgsConstructor
+@Component
+public class AdminDataInit {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    @Transactional
+    public void init() {
+
+        String password = bCryptPasswordEncoder.encode("qweasdzxc123");
+
+        LocalAccount localAccount =LocalAccount.builder()
+                .password(password)
+                .build();
+
+        User user = User.builder()
+                .email("admin@runto.com")
+                .name("관리자다")
+                .nickname("관리자다")
+                .gender(NONE)
+                .localAccount(localAccount)
+                .role(ADMIN)
+                .build();
+
+        userRepository.save(user);
+    }
+}
