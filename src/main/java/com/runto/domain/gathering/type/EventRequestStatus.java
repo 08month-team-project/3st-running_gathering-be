deb0054b09b7abce857e37ebafa0744a385ae6d2
유종화 <sola201@naver.com>
2024-11-06 00:52:30 +0900

    :sparkles: 이메일에 문자열을 사용하기 위해서 추가

--- src/main/java/com/runto/domain/gathering/type/EventRequestStatus.java
+++ src/main/java/com/runto/domain/gathering/type/EventRequestStatus.java
@@ -1,8 +1,15 @@
 package com.runto.domain.gathering.type;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum EventRequestStatus {
 
-    PENDING,      // 대기
-    APPROVED,     // 승인
-    REJECTED      // 승인거부
+    PENDING("대기"),      // 대기
+    APPROVED("승인"),     // 승인
+    REJECTED("거절");      // 승인거부
+
+    private final String description;
 }
