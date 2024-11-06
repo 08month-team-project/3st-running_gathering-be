3c402f693d93a8779eb8328b894beaf5e2d6b4a0
lsy28901 <tndus28901@gmail.com>
2024-11-06 19:26:40 +0900

    :sparkles: Feat(CHAT): 그룹 채팅방 관련 쿼리 추가

--- src/main/java/com/runto/domain/chat/dao/GroupChatRoomRepository.java
+++ src/main/java/com/runto/domain/chat/dao/GroupChatRoomRepository.java
@@ -1,8 +1,22 @@
 package com.runto.domain.chat.dao;
 
 import com.runto.domain.chat.domain.GroupChatRoom;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GroupChatRoomRepository extends JpaRepository<GroupChatRoom,Long> {
 
+    @Query("select gc from GroupChatRoom gc " +
+            "where gc.gathering.id = :gid")
+    GroupChatRoom findByGatheringId(@Param("gid") Long gatheringId);
+
+    @Query("select gcr " +
+            "from GroupChatRoom gcr " +
+            "join fetch GroupChatRoomUser gu on gu.groupChatRoom.id = gcr.id " +
+            "join fetch Gathering g on gcr.gathering.id = g.id " +
+            "where gu.user.id = :id ")
+    Slice<GroupChatRoom> findGroupChatRoomById(Long id, Pageable pageable);
 }
