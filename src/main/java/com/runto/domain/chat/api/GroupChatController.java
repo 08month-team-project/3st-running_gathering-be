ea026a77c7d6e1893d8ffa1bb3bb4ca1fd0013ec
lsy28901 <tndus28901@gmail.com>
2024-11-06 19:20:19 +0900

    :sparkles:  Feat(CHAT): GroupChatController 그룹 채팅방 생성, 참가 , 목록조회, 목록에서 채팅방 조회 api 매핑

--- src/main/java/com/runto/domain/chat/api/GroupChatController.java
+++ src/main/java/com/runto/domain/chat/api/GroupChatController.java
@@ -1,31 +1,55 @@
 package com.runto.domain.chat.api;
 
 
+import com.runto.domain.chat.application.GroupChatService;
+import com.runto.domain.chat.dto.ChatResponse;
+import com.runto.domain.chat.dto.ChatRoomResponse;
+import com.runto.domain.chat.dto.MessageResponse;
+import com.runto.global.security.detail.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chat/group")
 @RequiredArgsConstructor
 public class GroupChatController {
 
-    /**
-     * 그룹 채팅 생성, 참가 api 를 따로 만들까?
-     * !!따로 만들 경우
-     * 명확한 책임의 분리가 가능함.
-     * 클라이언트에게 두번 요청을 받아야함.
-     * 채팅방의 동시성 제어를 따로 해줘야함.
-     * 한번의 처리에 대한 데이터 처리량이 줄어든다.
-     *
-     * !!모임 서비스에서 한번에 처리할 경우
-     * 엔티티에 대한 접근(쿼리)이 줄어든다.
-     * 동시성처리는 모임 생성,참가에서 하면 채팅방도 동시성 해결
-     * 한번에 처리에 대한 데이터 처리량이 증가함.
-     * 채팅 생성 api 따로 없으므로 채팅 생성, 참가를 따로 하고싶을 경우의 기능의 확장성 없음
-     */
+    private final GroupChatService groupChatService;
 
-    //그룹 채팅 생성 api
+    //그룹 채팅 생성 api -> 생성자는 자동으로 참가되야함
+    @PostMapping
+    public void createGroupChat(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                @RequestParam("id") Long gatheringId){
+        groupChatService.createGroupChatRoom(gatheringId);
+        groupChatService.joinGroupChatRoom(userDetails.getUserId(), gatheringId);
+    }
 
     //그룹 채팅 참가 api
+    @PostMapping("/join")
+    public void joinGroupChat(@AuthenticationPrincipal CustomUserDetails userDetails,
+                              @RequestParam("id") Long gatheringId){
+        groupChatService.joinGroupChatRoom(userDetails.getUserId(), gatheringId);
+    }
+    //그룹 채팅방 목록 조회
+    @GetMapping("/list")
+    public ResponseEntity<Slice<ChatRoomResponse>> getGroupChatList(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                                    @RequestParam(name = "page_num") int pageNum,
+                                                                    @RequestParam(defaultValue = "7") int size){
+        return ResponseEntity.ok(groupChatService.getGroupChatRoomList(userDetails.getUserId(),pageNum ,size));
+    }
+    //그룹 채팅방 목록에서 조회
+    @GetMapping("/{room_id}")
+    public ResponseEntity<ChatResponse> getGroupChatRoom(@PathVariable("room_id") Long roomId,
+                                                         @RequestParam(name = "page_num") int pageNum,
+                                                         @RequestParam(defaultValue = "7") int size){
+        Slice<MessageResponse> messageResponses = groupChatService.getGroupChatRoom(roomId,pageNum,size);
+        ChatResponse chatResponse = ChatResponse.builder()
+                .roomId(roomId)
+                .messages(messageResponses).build();
+
+        return ResponseEntity.ok(chatResponse);
+    }
 }
