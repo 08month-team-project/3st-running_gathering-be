90206682341380860264837059f2ef860bf11167
유종화 <sola201@naver.com>
2024-11-08 20:17:36 +0900

    :sparkles: 조회용 페이지네이션 쿠폰 추가

--- src/main/java/com/runto/domain/coupon/dao/UserCouponsRepository.java
+++ src/main/java/com/runto/domain/coupon/dao/UserCouponsRepository.java
@@ -3,11 +3,15 @@ package com.runto.domain.coupon.dao;
 
 import com.runto.domain.coupon.domain.Coupon;
 import com.runto.domain.coupon.domain.UserCoupons;
+import com.runto.domain.coupon.type.CouponStatus;
 import com.runto.domain.user.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserCouponsRepository extends JpaRepository<UserCoupons, Long> {
 
+    Page<UserCoupons> findByStatus(CouponStatus status, Pageable pageable);
 
     boolean existsByUserAndCoupon(User user, Coupon coupon);
 }
