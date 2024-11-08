31e82093b3594091c26e4ad94861b221624978c5
lsy28901 <tndus28901@gmail.com>
2024-11-09 04:07:49 +0900

    :bug: Fix(CHAT): 인터셉터 복구 및 SockJS 복구

--- src/main/java/com/runto/domain/chat/config/WebSocketConfig.java
+++ src/main/java/com/runto/domain/chat/config/WebSocketConfig.java
@@ -46,8 +46,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setAllowedOrigins("http://localhost:3000", serverName)
 //                .setAllowedOrigins("https://runto.vercel.app/")
                 .addInterceptors(new SocketInterceptor(jwtUtil))
-//                .withSockJS()
-//                .setClientLibraryUrl("https://cdn.jsdelivr.net/sockjs/1.6.1/sockjs.min.js")
+                .withSockJS()
+                .setClientLibraryUrl("https://cdn.jsdelivr.net/sockjs/1.6.1/sockjs.min.js")
                 ;
     }
 
