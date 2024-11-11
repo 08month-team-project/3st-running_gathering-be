1597b184609e5d40f132f81086f66ab81d3459c2
DokyungYou <willow74rr@gmail.com>
2024-11-11 23:38:35 +0900

    ♻️Refactor: api 세팅 데이터 코드 수정

--- src/main/java/com/runto/test_api/TestDataInit.java
+++ src/main/java/com/runto/test_api/TestDataInit.java
@@ -40,6 +40,7 @@ public class TestDataInit {
     private final GatheringRepository gatheringRepository;
     private final GatheringMemberCountRepository gatheringMemberCountRepository;
 
+
     @EventListener(ApplicationReadyEvent.class)
     @Transactional
     public void init() {
@@ -58,14 +59,14 @@ public class TestDataInit {
                     .build();
 
             User user = User.builder()
-                    .email("runto" + i + "@gmail.com")
+                    .email("runtorun" + i + "@gmail.com")
                     .name("테스트유저이름" + i)
-                    .nickname("테스트유저닉네임" + i)
+                    .nickname("runtorun" + i)
                     .gender(MAN)
                     //.status()
                     .localAccount(localAccount)
                     .role(UserRole.USER)
-                    .profileImageUrl(i+ "번유저 썸네일 url")
+                    .profileImageUrl(i + "번유저 썸네일 url")
                     .build();
 
             users.add(user);
@@ -82,24 +83,30 @@ public class TestDataInit {
                         new Coordinates(new BigDecimal("0.0"), new BigDecimal("0.0")));
 
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             Gathering gathering = Gathering.builder()
                     .organizerId(users.get(i).getId())
-                    .title("(기한 남음) 우리 모두 달립시다 - runto" + (i+1) + "유저")
+                    .title("(기한 남음) 우리 모두 달립시다 - runto" + (i + 1) + "유저")
                     .description("재밌게 달려봅시다.")
                     .appointedAt(LocalDateTime.now().plusDays(i + 2))
                     .deadline(LocalDateTime.now().plusDays(i + 1))
                     .concept(RunningConcept.HEALTH)
                     .goalDistance(GoalDistance.FREE)
                     .thumbnailUrl(i + "번 모임 썸네일")
-                    //.hits(0)
+                    .hits(0L)
                     .location(location)
                     .status(NORMAL)
-                    .maxNumber(6)
-                    //.currentNumber(0)
+                    .maxNumber(10)
+                    .currentNumber(0)
                     .gatheringType(GatheringType.GENERAL)
                     .build();
             gathering.addMember(users.get(i), ORGANIZER);
+            gathering.updateCurrentNumber(1);
+
+            // 이벤트로 세팅하고싶다면
+            // gatheringType(GatheringType.EVENT) 로 바꾸고 아래 코드 추가
+            //gathering.applyForEvent();
+
 
             for (int j = 0; j < 10; j++) {
                 if (j == i) continue;
@@ -112,39 +119,7 @@ public class TestDataInit {
             gatherings.add(gathering);
         }
 
-//        for (int i = 0; i < 10; i++) {
-//            Gathering gathering = Gathering.builder()
-//                    .organizerId(users.get(i).getId())
-//                    .title("(기한 만료) 우리 모두 달립시다 - runto" + (i+1) + "유저")
-//                    .description("재밌게 달려봅시다." + i)
-//                    .appointedAt(LocalDateTime.now().minusDays(i + 2))
-//                    .deadline(LocalDateTime.now().minusDays(i + 1))
-//                    .concept(RunningConcept.HEALTH)
-//                    .goalDistance(GoalDistance.FREE)
-//                    .thumbnailUrl(i + "번 모임 썸네일")
-//                    //.hits(0)
-//                    .location(location)
-//                    .status(NORMAL)
-//                    .maxNumber(10)
-//                    //.currentNumber(0)
-//                    .gatheringType(GatheringType.EVENT)
-//                    .build();
-//            gathering.applyForEvent();
-//
-//            gathering.addMember(users.get(i), ORGANIZER);
-//
-//            for (int j = 0; j < 10; j++) {
-//                if (j == i) continue;
-//
-//                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
-//                    gathering.addMember(users.get(j), PARTICIPANT);
-//                }
-//            }
-//
-//            gatherings.add(gathering);
-//        }
-
-        gatheringRepository.saveAll(gatherings);
+        gatherings = gatheringRepository.saveAll(gatherings);
 
 
         List<GatheringMemberCount> gatheringMemberCounts = new ArrayList<>();
@@ -152,6 +127,5 @@ public class TestDataInit {
                 gatheringMemberCounts.add(GatheringMemberCount.from(gathering)));
 
         gatheringMemberCountRepository.saveAll(gatheringMemberCounts);
-
     }
 }
