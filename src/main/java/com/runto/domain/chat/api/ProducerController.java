832b38309cde817f1a879b9aaba8a41ca40eb341
lsy28901 <tndus28901@gmail.com>
2024-11-13 03:36:53 +0900

    :bug: Fix(CHAT): ProducerController 그룹 채팅도 StompHeaderAccessor 사용하도록 수정

--- src/main/java/com/runto/domain/chat/api/ProducerController.java
+++ src/main/java/com/runto/domain/chat/api/ProducerController.java
@@ -2,9 +2,7 @@ package com.runto.domain.chat.api;
 
 import com.runto.domain.chat.application.ProducerService;
 import com.runto.domain.chat.dto.MessageDTO;
-import com.runto.global.security.detail.CustomUserDetailService;
 import com.runto.global.security.detail.CustomUserDetails;
-import com.runto.global.security.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -14,13 +12,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import software.amazon.awssdk.services.s3.endpoints.internal.Value;
 
-import java.security.Principal;
 import java.time.LocalDateTime;
 
 
@@ -29,7 +24,6 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class ProducerController {
     private final ProducerService producerService;
-    private final JWTUtil jwtUtil;
 
     //1:1 채팅 웹소켓 메시지 전송
     @MessageMapping("/send/direct")
@@ -37,16 +31,13 @@ public class ProducerController {
         Authentication authentication = (Authentication) headerAccessor.getUser();
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         Long userId = userDetails.getUserId();
-//        SecurityContext context = SecurityContextHolder.getContext();
-//        log.info("ProducerController direct context = {}",context);
-//        Authentication authentication = context.getAuthentication();
+
         log.info("ProducerController direct authentication = {}",authentication);
-//        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
         log.info("ProducerController direct userDetails userId = {}",userDetails.getUserId());
         if (messageDTO.getTimestamp() == null){
             messageDTO.setTimestamp(LocalDateTime.now());
         }
-//        producerService.sendDirectMessage(messageDTO, userDetails.getUserId());
         producerService.sendDirectMessage(messageDTO,userId);
     }
 
@@ -60,30 +51,19 @@ public class ProducerController {
         producerService.sendDirectMessage(messageDTO, userDetails.getUserId());
     }
 
-//    //그룹 채팅 웹소켓 메시지 전송
-//    @MessageMapping("/send/group")
-//    public void sendGroupMessage(@Payload MessageDTO messageDTO,
-//                                  @AuthenticationPrincipal CustomUserDetails userDetails){
-//        log.info("ProducerController userDetails userId = {}",userDetails.getUserId());
-//        if (messageDTO.getTimestamp() == null){
-//            messageDTO.setTimestamp(LocalDateTime.now());
-//        }
-//        producerService.sendGroupMessage(messageDTO, userDetails.getUserId());
-//    }
-
     //그룹 채팅 웹소켓 메시지 전송
     @MessageMapping("/send/group")
-    public void sendGroupMessage(@Payload MessageDTO messageDTO){
-        SecurityContext context = SecurityContextHolder.getContext();
-        log.info("ProducerController group context = {}",context);
-        Authentication authentication = context.getAuthentication();
-        log.info("ProducerController group authentication = {}",authentication);
+    public void sendGroupMessage(@Payload MessageDTO messageDTO,StompHeaderAccessor headerAccessor){
+        Authentication authentication = (Authentication) headerAccessor.getUser();
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-        log.info("ProducerController group userDetails userId = {}",userDetails.getUserId());
+        Long userId = userDetails.getUserId();
+        log.info("ProducerController group authentication = {}",authentication);
+
+        log.info("ProducerController group userDetails userId = {}",userId);
         if (messageDTO.getTimestamp() == null){
             messageDTO.setTimestamp(LocalDateTime.now());
         }
-        producerService.sendGroupMessage(messageDTO, userDetails.getUserId());
+        producerService.sendGroupMessage(messageDTO, userId);
     }
 
     //그룹 채팅 테스트 메시지 전송 api
