f7749cdc405aade3b82d289c88f66174987e0052
lsy28901 <tndus28901@gmail.com>
2024-11-12 15:18:04 +0900

    :bug: Fix(CHAT): WebSocketConfig 수정 SecurityContext 직접 설정

--- src/main/java/com/runto/domain/chat/config/WebSocketConfig.java
+++ src/main/java/com/runto/domain/chat/config/WebSocketConfig.java
@@ -23,6 +23,7 @@ import org.springframework.retry.support.RetryTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -74,9 +75,43 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         return retryTemplate;
     }
 
+
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+//        registration.interceptors(new ChannelInterceptor() {
+//            @Override
+//            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+//                StompHeaderAccessor headerAccessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+//                String authHeader = headerAccessor.getFirstNativeHeader("Authorization");
+//
+//                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+//                    String token = authHeader.substring(7);
+//                    try {
+//                        Long userId = jwtUtil.getId(token);  // JWT에서 사용자 정보 추출
+//                        log.info("InboundChannel userId = {}",userId);
+//                        String username = jwtUtil.getUsername(token);
+//                        log.info("InboundChannel username = {}",username);
+//                        String role = jwtUtil.getRole(token);
+//
+//                        // 인증된 사용자 정보 설정
+//                        CustomUserDetails userDetails = new CustomUserDetails(new UserDetailsDTO(userId,null,null,null,username,null,role));
+//                        log.info("InboundChannel userDetails userId = {}",userDetails.getUserId());
+//                        Authentication authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, List.of(new SimpleGrantedAuthority(role)));
+//                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+//                        log.info("InboundChannel Context Authentication = {}",SecurityContextHolder.getContext().getAuthentication());
+//
+//                    } catch (Exception e) {
+//                        throw new RuntimeException("Invalid token: " + e.getMessage());
+//                    }
+//                }
+//                return message;
+//            }
+//        });
+//    }
+
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
         registration.interceptors(new ChannelInterceptor() {
             @Override
             public Message<?> preSend(Message<?> message, MessageChannel channel) {
@@ -86,18 +121,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 if (authHeader != null && authHeader.startsWith("Bearer ")) {
                     String token = authHeader.substring(7);
                     try {
-                        Long userId = jwtUtil.getId(token);  // JWT에서 사용자 정보 추출
-                        log.info("InboundChannel userId = {}",userId);
+                        Long userId = jwtUtil.getId(token);
                         String username = jwtUtil.getUsername(token);
-                        log.info("InboundChannel username = {}",username);
                         String role = jwtUtil.getRole(token);
 
-                        // 인증된 사용자 정보 설정
-                        CustomUserDetails userDetails = new CustomUserDetails(new UserDetailsDTO(userId,null,null,null,username,null,role));
-                        log.info("InboundChannel userDetails userId = {}",userDetails.getUserId());
+                        CustomUserDetails userDetails = new CustomUserDetails(new UserDetailsDTO(userId, null, null, null, username, null, role));
                         Authentication authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, List.of(new SimpleGrantedAuthority(role)));
-                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                        log.info("InboundChannel Context Authentication = {}",SecurityContextHolder.getContext().getAuthentication());
+                        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                        securityContext.setAuthentication(authenticationToken);
+
+                        // 스레드에서 SecurityContext를 직접 설정
+                        SecurityContextHolder.setContext(securityContext);
 
                     } catch (Exception e) {
                         throw new RuntimeException("Invalid token: " + e.getMessage());
@@ -107,6 +141,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
             }
         });
     }
+
 
 //    @Override
 //    public void configureClientInboundChannel(ChannelRegistration registration) {
