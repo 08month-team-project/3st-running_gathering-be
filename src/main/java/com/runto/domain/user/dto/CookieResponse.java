cb0f00c69855df3e9ff940bcf7a4dad8ef7681b5
TestSeung <sjlim589@hanmail.net>
2024-11-14 05:30:20 +0900

    :recycle: refactor: 쿠키변경

---
+++ src/main/java/com/runto/domain/user/dto/CookieResponse.java
@@ -0,0 +1,15 @@
+package com.runto.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CookieResponse {
+    private String token;
+    private String name;
+}
